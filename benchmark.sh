2184eff15c87ad06579e03661cda8135d702c986
Andre Mueller <foss@andremueller-online.de>
2018-07-03 11:11:10 +0200

    modified benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -4,5 +4,5 @@ echo "running CPU benchmark with genomic sequences"
 ./anyseq/build/align -i anyseq/sequences/sboydii.fna anyseq/sequences/ecoli.fna
 ./anyseq/build/align -i anyseq/sequences/rose.fna anyseq/sequences/caul.fna
 ./anyseq/build/align -i anyseq/sequences/ecoli.fna anyseq/sequences/caul.fna
-./anyseq/build/align -i anyseq/sequences/rattus.fna anyseq/sequences/rattus.fna
+./anyseq/build/align -i anyseq/sequences/rattus.fna anyseq/sequences/ecoli.fna
 
